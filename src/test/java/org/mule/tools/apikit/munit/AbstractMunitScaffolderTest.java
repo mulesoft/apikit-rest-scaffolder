164db59ea6bc20a70e4c08acdd774b70d742fe28
Martin Cousido <martincousido@gmail.com>
2019-08-20 17:45:58 -0300

    Fixing imports

--- src/test/java/org/mule/tools/apikit/munit/AbstractMunitScaffolderTest.java
+++ src/test/java/org/mule/tools/apikit/munit/AbstractMunitScaffolderTest.java
@@ -14,7 +14,13 @@ import org.mule.parser.service.ParserService;
 import org.mule.parser.service.result.ParseResult;
 import org.mule.tools.apikit.MunitScaffolder;
 import org.mule.tools.apikit.TestUtils;
-import org.mule.tools.apikit.model.*;
+import org.mule.tools.apikit.model.MuleConfig;
+import org.mule.tools.apikit.model.MuleConfigBuilder;
+import org.mule.tools.apikit.model.MunitScaffolderContext;
+import org.mule.tools.apikit.model.RuntimeEdition;
+import org.mule.tools.apikit.model.ScaffolderContextBuilder;
+import org.mule.tools.apikit.model.ScaffoldingConfiguration;
+import org.mule.tools.apikit.model.ScaffoldingResult;
 
 import java.io.File;
 
