1b6f38dc4805eacf9583e6f2f51c15bbe69717bd
Diego Martin Strubolini <dstrubolini@internal.salesforce.com>
2019-10-01 18:10:03 -0300

    wip

--- src/test/java/org/mule/tools/apikit/munit/MunitScaffolderErrors.java
+++ src/test/java/org/mule/tools/apikit/munit/MunitScaffolderErrors.java
@@ -12,6 +12,9 @@ import org.mule.tools.apikit.model.ScaffoldingResult;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
+import static org.mule.tools.apikit.MunitScaffolder.*;
+import static org.mule.tools.apikit.MunitScaffolder.NO_APIKIT_CONFIGS_ERROR_TEMPLATE;
+import static org.mule.tools.apikit.MunitScaffolder.NO_MAIN_FLOWS_ERROR_TEMPLATE;
 
 public class MunitScaffolderErrors extends AbstractMunitScaffolderTest {
 
@@ -21,30 +24,28 @@ public class MunitScaffolderErrors extends AbstractMunitScaffolderTest {
   @Test
   public void twoApikitConfigsReferencingTheSameAPI() throws Exception {
     String muleConfigLocation = "two-apikit-configs-referencing-same-raml.xml";
-    String fullPath = Thread.currentThread().getContextClassLoader().getResource("scaffolder/simple.yaml").getPath();
-    String expectedErrorMessage = String.format(MunitScaffolder.MULTIPLE_APIKIT_CONFIGS_ERROR_TEMPLATE, fullPath);
+    String expectedErrorMessage = String.format(MULTIPLE_APIKIT_CONFIGS_ERROR_TEMPLATE, "scaffolder/simple.yaml");
     makeAssertion(muleConfigLocation, expectedErrorMessage);
   }
 
   @Test
   public void twoMainFlowsReferencingTheSameConfig() throws Exception {
     String muleConfigLocation = "two-main-flows-referencing-same-config.xml";
-    String expectedErrorMessage = String.format(MunitScaffolder.MULTIPLE_MAIN_FLOWS_ERROR_TEMPLATE, "api-config");
+    String expectedErrorMessage = String.format(MULTIPLE_MAIN_FLOWS_ERROR_TEMPLATE, "api-config");
     makeAssertion(muleConfigLocation, expectedErrorMessage);
   }
 
   @Test
   public void noApikitConfigFound() throws Exception {
     String muleConfigLocation = "missing-apikit-config-for-raml.xml";
-    String fullPath = Thread.currentThread().getContextClassLoader().getResource("scaffolder/simple.yaml").getPath();
-    String expectedErrorMessage = String.format(MunitScaffolder.NO_APIKIT_CONFIGS_ERROR_TEMPLATE, fullPath);
+    String expectedErrorMessage = String.format(NO_APIKIT_CONFIGS_ERROR_TEMPLATE, "scaffolder/simple.yaml");
     makeAssertion(muleConfigLocation, expectedErrorMessage);
   }
 
   @Test
   public void noMainFlowForApikitConfig() throws Exception {
     String muleConfigLocation = "missing-main-flow-for-apikit-config.xml";
-    String expectedErrorMessage = String.format(MunitScaffolder.NO_MAIN_FLOWS_ERROR_TEMPLATE, "api-config");
+    String expectedErrorMessage = String.format(NO_MAIN_FLOWS_ERROR_TEMPLATE, "api-config");
     makeAssertion(muleConfigLocation, expectedErrorMessage);
   }
 
