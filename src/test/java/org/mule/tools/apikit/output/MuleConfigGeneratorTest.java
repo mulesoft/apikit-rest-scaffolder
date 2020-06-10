732f785ae6be324b06c7a8788f20cdf00a5aa751
Nicolas Kosacoff <nicolas.kosacoff@mulesoft.com>
2020-06-10 15:25:58 -0300

    APIKIT-2638: Modularize assertions on test.

--- src/test/java/org/mule/tools/apikit/output/MuleConfigGeneratorTest.java
+++ src/test/java/org/mule/tools/apikit/output/MuleConfigGeneratorTest.java
@@ -98,24 +98,9 @@ public class MuleConfigGeneratorTest {
 
   @Test
   public void blankDocumentWithoutLCInDomain() {
-
     Document document = this.scaffoldBlankDocument(SHOW_CONSOLE);
-
     Element rootElement = document.getRootElement();
-    assertEquals("mule", rootElement.getName());
-    Element xmlListenerConfig = rootElement.getChildren().get(0);
-    assertEquals("listener-config", xmlListenerConfig.getName());
-
-    Element apikitConfig = rootElement.getChildren().get(1);
-    assertEquals("hello-config", apikitConfig.getAttribute("name").getValue());
-
-    Element mainFlow = rootElement.getChildren().get(2);
-
-    assertEquals("flow", mainFlow.getName());
-    assertEquals("hello-main", mainFlow.getAttribute("name").getValue());
-    assertEquals("httpListenerConfig", mainFlow.getChildren().get(0).getAttribute("config-ref").getValue());
-    assertEquals("/api/*", mainFlow.getChildren().get(0).getAttribute("path").getValue());
-
+    assertCommonFlows(rootElement);
     Element consoleFlow = rootElement.getChildren().get(3);
     assertEquals("flow", consoleFlow.getName());
     assertEquals("hello-console", consoleFlow.getAttribute("name").getValue());
@@ -124,11 +109,15 @@ public class MuleConfigGeneratorTest {
     assertEquals("console", consoleFlow.getChildren().get(1).getName());
   }
 
-
   @Test
   public void blankDocumentWithoutLCInDomainHideConsole() {
     Document document = scaffoldBlankDocument(HIDE_CONSOLE);
     Element rootElement = document.getRootElement();
+    assertCommonFlows(rootElement);
+
+  }
+
+  private void assertCommonFlows(Element rootElement) {
     assertEquals("mule", rootElement.getName());
     Element xmlListenerConfig = rootElement.getChildren().get(0);
     assertEquals("listener-config", xmlListenerConfig.getName());
@@ -142,7 +131,6 @@ public class MuleConfigGeneratorTest {
     assertEquals("hello-main", mainFlow.getAttribute("name").getValue());
     assertEquals("httpListenerConfig", mainFlow.getChildren().get(0).getAttribute("config-ref").getValue());
     assertEquals("/api/*", mainFlow.getChildren().get(0).getAttribute("path").getValue());
-
   }
 
   private Document scaffoldBlankDocument(boolean showConsole) {
