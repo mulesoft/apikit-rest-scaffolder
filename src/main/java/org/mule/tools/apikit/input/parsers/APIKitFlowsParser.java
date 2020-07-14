7b8c62b560efff4649e1174f29a13b930cffd9f1
JuanAller <juan.aller@mulesoft.com>
2020-07-14 17:51:27 -0300

    APIKIT-2558: Scaffolding error No APIKit entries found in Mule config (#88)

--- src/main/java/org/mule/tools/apikit/input/parsers/APIKitFlowsParser.java
+++ src/main/java/org/mule/tools/apikit/input/parsers/APIKitFlowsParser.java
@@ -8,12 +8,9 @@ package org.mule.tools.apikit.input.parsers;
 
 import org.jdom2.Document;
 import org.jdom2.Element;
-import org.jdom2.filter.Filters;
 import org.jdom2.xpath.XPathExpression;
-import org.jdom2.xpath.XPathFactory;
 import org.mule.tools.apikit.input.APIKitFlow;
 import org.mule.tools.apikit.misc.APIKitTools;
-import org.mule.tools.apikit.misc.FlowNameUtils;
 import org.mule.tools.apikit.model.ApikitMainFlowContainer;
 import org.mule.tools.apikit.model.ResourceActionMimeTypeTriplet;
 import org.slf4j.Logger;
@@ -24,6 +21,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import static java.lang.String.format;
+import static org.jdom2.filter.Filters.element;
+import static org.jdom2.xpath.XPathFactory.instance;
+import static org.mule.tools.apikit.input.APIKitFlow.buildFromName;
+import static org.mule.tools.apikit.misc.FlowNameUtils.decode;
 import static org.mule.tools.apikit.output.MuleConfigGenerator.XMLNS_NAMESPACE;
 
 public class APIKitFlowsParser implements MuleConfigFileParser<Set<ResourceActionMimeTypeTriplet>> {
@@ -32,40 +34,36 @@ public class APIKitFlowsParser implements MuleConfigFileParser<Set<ResourceActio
 
   private final Map<String, ApikitMainFlowContainer> includedApis;
 
-  public APIKitFlowsParser(final Map<String, ApikitMainFlowContainer> includedApis) {
+  public APIKitFlowsParser(Map<String, ApikitMainFlowContainer> includedApis) {
     this.includedApis = includedApis;
   }
 
   @Override
   public Set<ResourceActionMimeTypeTriplet> parse(Document document) {
     Set<ResourceActionMimeTypeTriplet> entries = new HashSet<>();
-    XPathExpression<Element> xp = XPathFactory.instance().compile("//*/*[local-name()='flow']",
-                                                                  Filters.element(XMLNS_NAMESPACE.getNamespace()));
+    XPathExpression<Element> xp = instance().compile("//*/*[local-name()='flow']",
+                                                     element(XMLNS_NAMESPACE.getNamespace()));
     List<Element> elements = xp.evaluate(document);
     for (Element element : elements) {
-      String name = FlowNameUtils.decode(element.getAttributeValue("name"));
-      APIKitFlow flow;
+      String name = decode(element.getAttributeValue("name"));
       try {
-        flow = APIKitFlow.buildFromName(name, includedApis.keySet());
-      } catch (IllegalArgumentException iae) {
-        LOGGER.info("Flow named '" + name + "' is not an APIKit Flow because it does not follow APIKit naming convention.");
-        continue;
-      }
+        APIKitFlow flow = buildFromName(name, includedApis.keySet());
+        ApikitMainFlowContainer api = includedApis.get(flow.getConfigRef());
 
-      ApikitMainFlowContainer api = includedApis.get(flow.getConfigRef());
-
-      String resource = flow.getResource();
-      if (api != null) {
-        if (!resource.startsWith("/")) {
-          resource = "/" + resource;
+        if (api == null) {
+          throw new IllegalStateException(format("No APIKit configuration found for flow %s", name));
         }
+
         if (api.getPath() == null) {
           throw new IllegalStateException("Api path is invalid");
         }
+
+        String resource = flow.getResource().startsWith("/") ? flow.getResource() : "/" + flow.getResource();
         String path = APIKitTools.getCompletePathFromBasePathAndPath(api.getHttpListenerConfig().getBasePath(), api.getPath());
         entries.add(new ResourceActionMimeTypeTriplet(api, path + resource, flow.getAction(), flow.getMimeType()));
-      } else {
-        throw new IllegalStateException("No APIKit entries found in Mule config");
+
+      } catch (IllegalArgumentException e) {
+        LOGGER.info("Flow named '" + name + "' is not an APIKit Flow because it does not follow APIKit naming convention.");
       }
     }
     return entries;
