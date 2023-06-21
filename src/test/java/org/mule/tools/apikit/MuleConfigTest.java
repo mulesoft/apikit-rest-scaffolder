261f612c17ee985a58bb2c5b6b09555c6abe57e9
Juan Brasca <juan.brasca@gmail.com>
2023-06-21 17:07:38 -0300

    W-13577593: update dependencies for java 17 support (#230) (#232)

--- src/test/java/org/mule/tools/apikit/MuleConfigTest.java
+++ src/test/java/org/mule/tools/apikit/MuleConfigTest.java
@@ -34,7 +34,7 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.mule.apikit.model.ActionType.GET;
