b3afd7d6c436cc3fc7557c09856536ba564e3967
Juan Brasca <juan.brasca@gmail.com>
2023-07-17 17:47:48 -0300

    W-13206079: rescaffolding now preserves queryParametersStrictValidation atrribute (#245) (#246)

--- src/main/java/org/mule/tools/apikit/model/APIKitConfig.java
+++ src/main/java/org/mule/tools/apikit/model/APIKitConfig.java
@@ -6,9 +6,16 @@
  */
 package org.mule.tools.apikit.model;
 
+import org.jdom2.Attribute;
 import org.jdom2.Element;
 import org.mule.tools.apikit.output.scopes.APIKitConfigScope;
 import org.mule.tools.apikit.output.scopes.Scope;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class APIKitConfig implements Scope {
 
@@ -23,7 +30,18 @@ public class APIKitConfig implements Scope {
   public static final String HTTP_STATUS_VAR_ATTRIBUTE = "httpStatusVarName";
   public static final String DEFAULT_HTTP_STATUS_NAME = "httpStatus";
   public static final String DISABLE_VALIDATIONS = "disableValidations";
+  public static final String QUERY_PARAMS_STRICT_VALIDATION = "queryParamsStrictValidation";
+  public static final String HEADERS_STRICT_VALIDATION = "headersStrictValidation";
+  public static final String KEEP_API_BASE_URI = "keepApiBaseUri";
+  public static final String KEEP_RAML_BASE_URI = "keepRamlBaseUri";
 
+
+  public static final List<String> ADDITIONAL_ATTRIBUTES = Arrays.asList(
+                                                                         DISABLE_VALIDATIONS,
+                                                                         QUERY_PARAMS_STRICT_VALIDATION,
+                                                                         HEADERS_STRICT_VALIDATION,
+                                                                         KEEP_API_BASE_URI,
+                                                                         KEEP_RAML_BASE_URI);
 
   private String name;
   private String api;
@@ -31,7 +49,7 @@ public class APIKitConfig implements Scope {
   private Boolean extensionEnabled = null;
   private String outboundHeadersMapName = DEFAULT_OUTBOUND_HEADERS_MAP_NAME;
   private String httpStatusVarName = DEFAULT_HTTP_STATUS_NAME;
-  private String disableValidations;
+  private Map<String, String> additionalAttributes = new HashMap<>();
 
   public APIKitConfig(final String name,
                       final String api,
@@ -66,14 +84,6 @@ public class APIKitConfig implements Scope {
 
   public void setExtensionEnabled(Boolean enabled) {
     this.extensionEnabled = enabled;
-  }
-
-  public String getDisableValidations() {
-    return disableValidations;
-  }
-
-  public void setDisableValidations(String disableValidations) {
-    this.disableValidations = disableValidations;
   }
 
   public void setName(String name) {
@@ -113,6 +123,14 @@ public class APIKitConfig implements Scope {
 
   public String getApiSpecificationLocation() {
     return api != null ? api : raml;
+  }
+
+  public void addAdditionalAttribute(Attribute attribute) {
+    additionalAttributes.put(attribute.getName(), attribute.getValue());
+  }
+
+  public Set<Map.Entry<String, String>> getAdditionalAttributes() {
+    return additionalAttributes.entrySet();
   }
 
   public Element generate() {
